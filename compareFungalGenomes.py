061248ba1b5dc72867e96463a3be91ee25306730
Ant-28 <ananthajit5d@gmail.com>
2022-05-23 12:32:19 -0700

    Initialized fungalgenome class

--- compareFungalGenomes.py
+++ compareFungalGenomes.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 # Group Members: Ananthajit ("Ananth") Srikanth (asrikan1), Layla Myers (laamyers), Teresa Joseph (tkjoseph)
 # Required modules: sys
+
+
+class fungalGenome:
+    ITS1s = []
+    ITS2s = [] 
+    pass
