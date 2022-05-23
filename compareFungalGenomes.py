71945a0cfe765468f9fe9ea409383e73aabb2917
Ant-28 <ananthajit5d@gmail.com>
2022-05-23 12:41:21 -0700

    fixed comment, imported biopython

--- compareFungalGenomes.py
+++ compareFungalGenomes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Group Members: Ananthajit ("Ananth") Srikanth (asrikan1), Layla Myers (laamyers), Teresa Joseph (tkjoseph)
-# Required modules: sys
 
+import Bio
 
 class fungalGenome:
     ITS1s = []
